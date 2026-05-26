b796565a1d347083b3d9fecca142eedc625c9df5
Naresh Gantala <nareshgantala@gmail.com>
2026-05-26 13:20:51 +0530

    fix

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -39,7 +39,7 @@ resource "aws_route_table" "main_gw" {
 
 resource "aws_route_table_association" "main" {
   count = 2
-  subnet_id      = aws_subnet.public_subnet[count.index+1].id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.main_gw.id
 }
 
@@ -80,7 +80,7 @@ resource "aws_route_table" "main_nat" {
 
 resource "aws_route_table_association" "main_nat" {
   count = 2
-  subnet_id      = aws_subnet.private_subnet[count.index+1].id
+  subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.main_nat.id
 }
 
