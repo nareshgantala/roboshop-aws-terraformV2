69f475f3183fd4b52518582328376418e9b79d9d
Naresh Gantala <nareshgantala@gmail.com>
2026-05-26 13:46:40 +0530

    removed ttl

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -2,7 +2,6 @@ resource "aws_route53_record" "main" {
   zone_id = "Z09762381HTCZ4LICCNHQ"
   name    = var.component
   type    = "A"
-  ttl     = 300
     alias {
     name                   = var.record
     zone_id                = var.alb_zone_id
