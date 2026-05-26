2c5ed9f9173758dead8166959e7a2d357601a546
Naresh Gantala <nareshgantala@gmail.com>
2026-05-26 12:40:13 +0530

    update backend

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    backend "s3" {
+    bucket = "roboshop-aws-terraform"
+    key    = "infra-alb/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
