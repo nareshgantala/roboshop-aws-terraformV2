99e135b9839356e80fee520a5de1b740fd9db101
Naresh Gantala <nareshgantala@gmail.com>
2026-05-26 17:42:27 +0530

    update nat

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -69,7 +69,7 @@ resource "aws_route_table" "main_nat" {
 
         route {
             cidr_block = "0.0.0.0/0"
-            gateway_id = aws_nat_gateway.main.id
+            nat_gateway_id = aws_nat_gateway.main.id
         }
 
         tags = {
