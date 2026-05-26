18c0e648ae87a5ba2b5d6c7399548e773b8412a9
Naresh Gantala <nareshgantala@gmail.com>
2026-05-26 17:58:34 +0530

    all

--- variables.tf
+++ variables.tf
@@ -13,34 +13,34 @@ variable "ui" {
 variable "app" {
       default = {
 
-        # cart = {
-        #   instance_type = "t2.micro"
-        #   port = 8003
-        # }
-        # shipping = {
-        #   instance_type = "t2.micro"
-        #   port = 8004
-        # }
-        # payment = {
-        #   instance_type = "t2.micro"
-        #   port = 8005
-        # }
-        # orders = {
-        #   instance_type = "t2.micro"
-        #   port = 8007
-        # }
-        # ratings = {
-        #   instance_type = "t2.micro"
-        #   port = 8006
-        # }
+        cart = {
+          instance_type = "t2.micro"
+          port = 8003
+        }
+        shipping = {
+          instance_type = "t2.micro"
+          port = 8004
+        }
+        payment = {
+          instance_type = "t2.micro"
+          port = 8005
+        }
+        orders = {
+          instance_type = "t2.micro"
+          port = 8007
+        }
+        ratings = {
+          instance_type = "t2.micro"
+          port = 8006
+        }
         catalogue = {
           instance_type = "t2.micro"
           port = 8002
         }
-        # user = {
-        #   instance_type = "t2.micro"
-        #   port = 8001
-        # }
+        user = {
+          instance_type = "t2.micro"
+          port = 8001
+        }
     }
 }
 
