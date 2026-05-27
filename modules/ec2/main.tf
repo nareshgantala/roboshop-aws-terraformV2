3717ff6ca6dc170a8bcec3dc43708057c6214960
Naresh Gantala <nareshgantala@gmail.com>
2026-05-27 22:34:10 +0530

    update ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "db_instance" {
   instance_type = var.instance_type
   subnet_id = var.subnet_id
   vpc_security_group_ids = [var.sg]
+  key_name               = "roboshop_pem"
   tags = {
     Name = "roboshop-${var.component}"
   }
