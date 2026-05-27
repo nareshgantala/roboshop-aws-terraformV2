2ca533af296bc6d8bb17bc436227a998c05f9b43
Naresh Gantala <nareshgantala@gmail.com>
2026-05-27 19:38:45 +0530

    listner moved out of app components

--- main.tf
+++ main.tf
@@ -64,4 +64,15 @@ resource "aws_lb" "internal_alb" {
   }
 }
 
+resource "aws_lb_listener" "Internal_listener" {
+  load_balancer_arn = aws_lb.internal_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+ 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
 
--- modules/app_components/main.tf
+++ modules/app_components/main.tf
@@ -5,16 +5,7 @@ resource "aws_lb_target_group" "main"{
   vpc_id   = var.vpc_id
 }
 
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = var.load_balancer_arn
-  port              = "80"
-  protocol          = "HTTP"
- 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
+
 
 resource "aws_launch_template" "app_main" {
 
