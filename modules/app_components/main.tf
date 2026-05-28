bb346755f66f9a9ce52354fbdd4d9023797c6426
Naresh Gantala <nareshgantala@gmail.com>
2026-05-28 20:11:54 +0530

    added health check

--- modules/app_components/main.tf
+++ modules/app_components/main.tf
@@ -52,6 +52,9 @@ resource "aws_autoscaling_group" "app_main" {
   max_size           = 2
   min_size           = 1
 
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
   launch_template {
     id      = aws_launch_template.app_main.id
     version = "$Latest"
