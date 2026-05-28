225b007b1a9df79ba4931ba338131dd183e13e91
Naresh Gantala <nareshgantala@gmail.com>
2026-05-28 18:16:51 +0530

    add 22 port for catalogue

--- modules/securityGroups/main.tf
+++ modules/securityGroups/main.tf
@@ -149,6 +149,16 @@ resource "aws_security_group" "catalogue_sg" {
   }
 }
 
+resource "aws_security_group_rule" "catalogue_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.catalogue_sg.id
+}
+
+
 resource "aws_security_group" "user_sg" {
   name = "roboshop-user-sg"
   vpc_id = var.vpc_id
